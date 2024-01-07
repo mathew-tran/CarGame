7091b7506126f41cf315edbcf156e196e0f6e0c3
Mathew <mathew.tran.dev@gmail.com>
2024-01-06 19:14:06 -0800

    work on it a bit more, blocks now get colored in when car runs into it

---
+++ Assets/Scripts/HitCube.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCube : MonoBehaviour
+{
+    private Color[] OnHitColors = { Color.green, Color.blue, Color.cyan, Color.red, Color.magenta };
+
+    private bool bCanHit = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (bCanHit)
+        {
+            if (other.CompareTag("Car"))
+            {
+                GetComponent<Renderer>().material.color = OnHitColors[Random.Range(0, OnHitColors.Length)];
+                bCanHit = false;
+            }
+        }
+     
+    }
+}
